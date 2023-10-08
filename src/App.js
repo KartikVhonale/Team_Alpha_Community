0b5e6b0cd816d5fdc8075c7a54b70e34256061ac
Milind241 <milinddadore123@gmail.com>
2023-10-08 15:08:25 +0530

    added a comment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
+import Contest from './components/contest'
 import './App.css';
-
+import Login from './components/loginpage'
+import Chatbox from './components/chatbox'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contest />
+      <Login/>
+      <Chatbox/>
     </div>
   );
 }
---
+++ src/components/chatbox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/style_chatbox.css'
+
+const chatbox = () => {
+  return (
+    <div className='chatbox_main_box'>
+      <h1>this is me</h1>
+    </div>
+  )
+}
+
+export default chatbox
